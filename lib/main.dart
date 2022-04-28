9bcc1d2d664d39205a6d194ceeb76e0fdfe5124b
Antoine Lestrade <antoine.lestrade@he-arc.ch>
2022-04-28 12:39:18 +0200

    ObjectBox class + FoodItem class

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,24 @@ import 'package:path_provider/path_provider.dart';
 
 import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:snaptoo/objectbox.g.dart';
 
 import 'package:tuple/tuple.dart';
+
+import 'Collections/DataModels/FoodCollectionItem.dart';
+import 'Collections/ObjectBox.dart';
 
 // Returns an image asset in the form of a File object.
 Future<File> getImageFileFromAssets(String path) async {
   final byteData = await rootBundle.load('assets/$path');
-  final file = await File('${(await getTemporaryDirectory()).path}/$path').create(recursive: true);
+  final file = await File('${(await getTemporaryDirectory()).path}/$path')
+      .create(recursive: true);
 
-  if (await Permission.storage.request().isGranted) {
-    await file.writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+  if (await Permission.storage
+      .request()
+      .isGranted) {
+    await file.writeAsBytes(byteData.buffer.asUint8List(
+        byteData.offsetInBytes, byteData.lengthInBytes));
   }
 
   return file;
@@ -25,7 +33,6 @@ Future<File> getImageFileFromAssets(String path) async {
 
 // Returns all the labels along with the index and confidence for given image.
 Future<List<Tuple3<String, int, double>>> getImageLabels(File file) async {
-
   List<Tuple3<String, int, double>> listLabel = [];
 
   // Create an InputImage
@@ -50,13 +57,31 @@ Future<void> main() async {
   runApp(const MyApp());
 
   // Create a File that will then be used to create an ML-Kit's InputImage.
-  File file = await getImageFileFromAssets('images/bus.jpg');                         // FOR SOME TESTING, THERE 3 DIFFERENT IMAGES IN THE ASSETS
+  File file = await getImageFileFromAssets(
+      'images/bus.jpg'); // FOR SOME TESTING, THERE 3 DIFFERENT IMAGES IN THE ASSETS
 
   // Retrieve the labels ML-Kit detects for a given image file.
   List<Tuple3<String, int, double>> listLabel = await getImageLabels(file);
 
   // JUSTE UN PETIT PRINT POUR TESTER
   print('\n\nLES LABELS DETECTES POUR L\'IMAGE CHOISIE : $listLabel\n\n\n');
+
+
+  final objectBox = await ObjectBox.create();
+  final foodBox = objectBox.Food();
+
+  for (var element in listLabel) {
+    foodBox.put(FoodCollectionItem(labelName: element.item1, score: element.item2, imagePath: "default path"));
+  }
+
+  List<FoodCollectionItem> foodItemList = foodBox.getAll();
+  print('\n\nItems stockés dans la DB : \n\n');
+  for (var element in foodItemList) {
+    print('$element.toString() \n');
+  }
+
+  foodBox.removeAll();
+  objectBox.Close();
 }
 
 class MyApp extends StatelessWidget {
@@ -69,7 +94,8 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Image from assets"),
         ),
-        body: Image.asset('assets/images/bus.jpg'),                                   // FOR SOME TESTING, THERE 3 DIFFERENT IMAGES IN THE ASSETS
+        body: Image.asset(
+            'assets/images/bus.jpg'), // FOR SOME TESTING, THERE 3 DIFFERENT IMAGES IN THE ASSETS
       ),
     );
   }

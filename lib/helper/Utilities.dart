288466b7fe47fa99bb9f260d1d1159ac2a4d3b4d
Guillaume 'Myrmillion' D <>
2022-05-03 15:46:32 +0200

    some changes (not so important really)

--- lib/helper/Utilities.dart
+++ lib/helper/Utilities.dart
@@ -1,4 +1,6 @@
 class Utilities {
+  // This is simply a "smart" implementation of a regular switch-case that is directly usable as is
+  // in the Scaffold code area.
   static TValue? customCase<TOptionType, TValue>(
       TOptionType selectedOption, Map<TOptionType, TValue> branches) {
     return branches[selectedOption];
---
+++ lib/my_app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'collections/ObjectBox.dart';
+import 'home/view/home_view.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key, required this.objectBox}) : super(key: key);
+
+  final ObjectBox objectBox;
+
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: widget.objectBox,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          HomeView.route: (context) => const HomeView(),
+        },
+        initialRoute: HomeView.route,
+      ),
+    );
+  }
+}

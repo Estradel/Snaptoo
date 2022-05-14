201df13a867ba94fbca04eee4071c2b14ca99e47
Guillaume 'Myrmillion' D <>
2022-05-14 13:08:23 +0200

    some other cool stuffs

--- lib/validation/bloc/validation_bloc.dart
+++ lib/validation/bloc/validation_bloc.dart
@@ -29,7 +29,7 @@ class ValidationBloc extends Bloc<ValidationEvent, ValidationState> {
     // Emit the state that will make the interface wait for resizing + labelling
     emit(const ImageAnalyzing(putCircle: false));
     // NECESSARY for previous State to have time to be emitted
-    await Future.delayed(const Duration(milliseconds: 10));
+    await Future.delayed(const Duration(milliseconds: 250));
 
     // Firstly get the labels of the image (with ML-Kit !)
     var listLabel = await ImageLabeler.getImageLabels(File(event.pickedFile.path));
